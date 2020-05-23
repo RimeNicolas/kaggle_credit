8e792a25a552705d9696d5d4cc13524c886f1acd
Nicolas Rime <n.rime@hotmail.com>
2020-05-23 16:04:30 +0200

    3 models tested

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # kaggle_credit
 Credit Card fraud analysis
+Dataset link: https://www.kaggle.com/mlg-ulb/creditcardfraud
+
+# Goal
+Compare 3 different models: Random Forests, Adaboost and Gradient Boost
+using the dataset linked above.
+The number of estimators, the maximum depth (for Random Forests only) and 
+the learning rate (for Adaboost and Gradient boost only) were varied over 
+a range of different values. The other parameters were left as the default
+of scikit-learn library. The three important metrics are accuracy (maximize
+correct predictions), number of false negative (avoid missing the detection
+of a fraudulent transaction) and Area Under the Curve (AUC).
+
+# Key takeaway
+1) The dataset is stronlgy unbalanced: 99.83% of the transactions are not fraudulent.
+Training on the full dataset takes a lot of time, only Random Forest and Adaboost
+were tested, Random Forest giving better results in term of accury, number of false
+negative and AUC, probably due to bagging.
+
+2) When a subset of not fraudulent transaction is taken in order to have a balanced 
+dataset (50% of fraudulent transactions), the 3 models were tested, in a much shorter time.
+Adaboost gave the best results, again in term of accury, number of false
+negative and AUC, achieving at best perfect accuracy on the validation set.
--- test_models.py
+++ test_models.py
@@ -9,6 +9,7 @@ from collections import Counter
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
@@ -19,6 +20,7 @@ from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
 
 def load_data(dir_data, csv_name):
     return pd.read_csv(os.path.join(dir_data, csv_name))
@@ -43,28 +45,53 @@ def prepare_data(dir_data, csv_name):
     y = np.array(df['Class'])
     return X, y
 
-def test_parameters(X, y, model, keys, params1, params2):
-    scores = np.zeros(len(params1) * len(params2))
-    tn = np.zeros(len(params1) * len(params2))
-    fp = np.zeros(len(params1) * len(params2))
-    fn = np.zeros(len(params1) * len(params2))
-    tp = np.zeros(len(params1) * len(params2))
-    f1 = np.zeros(len(params1) * len(params2))
-    auc = np.zeros(len(params1) * len(params2))
+def prepare_X_y(dataset_name, dir_data, balanced_dataset=False):
+    if dataset_name == 'cancer':
+        from sklearn import datasets
+        cancer = datasets.load_breast_cancer()
+        X = cancer.data
+        y = cancer.target
+    elif dataset_name == 'credit':
+        X, y = prepare_data(dir_data, 'creditcard.csv')
+        if balanced_dataset:
+            idx_is_fraud  = [i for i in range(len(y)) if y[i] == 1]
+            idx_not_fraud = [i for i in range(len(y)) if y[i] == 0]
+            X_is_fraud = X[idx_is_fraud]
+            y_is_fraud = y[idx_is_fraud]
+            X_not_fraud = X[idx_not_fraud]
+            y_not_fraud = y[idx_not_fraud]
+            np.random.seed(0)
+            idx_rand = np.random.randint(len(X_not_fraud), size=len(X_is_fraud))
+            X_not_fraud = X_not_fraud[idx_rand]
+            y_not_fraud = y_not_fraud[idx_rand]
+            X = np.concatenate((X_is_fraud, X_not_fraud), axis=0)
+            y = np.concatenate((y_is_fraud, y_not_fraud), axis=0)
+    else:
+        return None, None
+    return X, y
 
+def vary_parameters(X, y, model, dict_params):
+    keys = list()
+    for key in dict_params.keys():
+        keys.append(key)
+    params1 = dict_params[keys[0]]
+    params2 = dict_params[keys[1]]
+
+    acc, tn, fp, fn, tp, f1, auc = \
+        [np.zeros(len(params1) * len(params2)) for _ in range(7)]
+
+    nb_splits = 5
+    kwargs = dict()
+    kwargs['random_state'] = 0
     i = 0
     for param1 in params1:
         for param2 in params2:
-            nb_splits = 5
             cv = ShuffleSplit(n_splits=nb_splits, random_state=0)
-            #skf = StratifiedKFold(n_splits=nb_splits)
             clf_best = None
             score_best = 0
             for train_indexes, test_indexes in cv.split(X,y):
-                kwargs = dict()
                 kwargs[keys[0]] = param1
                 kwargs[keys[1]] = param2
-                kwargs['random_state'] = 0
                 clf = model(**kwargs)
                 clf.fit(X[train_indexes], y[train_indexes])
                 score = clf.score(X[test_indexes], y[test_indexes])
@@ -75,14 +102,14 @@ def test_parameters(X, y, model, keys, params1, params2):
             y_pred = clf_best.predict(X[test_indexes])
             y_score = clf_best.predict_proba(X[test_indexes])[:,1]
             y_true = y[test_indexes]
-            scores[i] = score_best
+            acc[i] = accuracy_score(y_true, y_pred)
             tn[i], fp[i], fn[i], tp[i] = confusion_matrix(y_true, y_pred).ravel()
             f1[i] = f1_score(y_true, y_pred)
             auc[i] = roc_auc_score(y_true, y_score)
             i+=1
 
     d = {
-        'accuracy':scores,
+        'accuracy':acc,
         'TP':tp,
         'FP':fp,
         'TN':tn,
@@ -94,47 +121,41 @@ def test_parameters(X, y, model, keys, params1, params2):
         'AUC':auc
     }
     df = pd.DataFrame(data=d)
+    df.insert(0, keys[0], np.repeat(params1, len(params2)), allow_duplicates=True)
+    df.insert(1, keys[1], (len(params1) * list(params2)), allow_duplicates=True)
     return df
 
-def prepare_X_y(dataset_name, dir_data):
-    if dataset_name == 'cancer':
-        from sklearn import datasets
-        cancer = datasets.load_breast_cancer()
-        X = cancer.data
-        y = cancer.target
-    elif dataset_name == 'credit':
-        X, y = prepare_data(dir_data, 'creditcard.csv')
-    else:
-        return
-    return X, y
-
-def test_parameters_random_forest(dataset_name, X,y):
-    model = RandomForestClassifier
-    model_str = 'Rand_Forest'
-    keys = ['n_estimators', 'max_depth']
-    nb_trees = np.arange(50,101,50)
-    max_depths = np.arange(6,13,2)
-    print('Testing parameters for model :', model_str)
-    df = test_parameters(X, y, model, keys, nb_trees, max_depths)
-    df.insert(0, 'model', model_str, allow_duplicates=True)
-    df.insert(1, keys[0], np.repeat(nb_trees, len(max_depths)), allow_duplicates=True)
-    df.insert(2, keys[1], (len(nb_trees) * list(max_depths)), allow_duplicates=True)
+def vary_parameters_model(X, y, kwargs):
+    print('Testing parameters for model :', kwargs['model_str'])
+    df = vary_parameters(X, y, kwargs['model'], kwargs['params'])
+    df.insert(0, 'model', kwargs['model_str'], allow_duplicates=True)
     timestr = time.strftime('%d_%m_%Y_%H_%M')
-    df.to_csv('_'.join(['./csv_dir/' + dataset_name, model_str, timestr + '.csv']), index=False)
+    df.to_csv('_'.join(['./csv_dir/' + kwargs['dataset_name'], 
+        kwargs['model_str'], timestr + '.csv']), index=False)
 
-def test_parameters_adaboost(dataset_name, X, y):
-    model = AdaBoostClassifier
-    model_str = 'AdaBoost'
-    keys = ['n_estimators', 'learning_rate']
-    n_estimators = np.arange(100,161,20)
-    learning_rates = np.arange(0.5,1.6,0.5)
-    print('Testing parameters for model :', model_str)
-    df = test_parameters(X, y, model, keys, n_estimators, learning_rates)
-    df.insert(0, 'model', model_str, allow_duplicates=True)
-    df.insert(1, keys[0], np.repeat(n_estimators, len(learning_rates)), allow_duplicates=True)
-    df.insert(2, keys[1], (len(n_estimators) * list(learning_rates)), allow_duplicates=True)
-    timestr = time.strftime('%d_%m_%Y_%H_%M')
-    df.to_csv('_'.join(['./csv_dir/' + dataset_name, model_str, timestr + '.csv']), index=False)
+def vary_parameters_random_forest(X, y, dataset_name, dict_params):
+    kwargs = dict()
+    kwargs['dataset_name'] = dataset_name
+    kwargs['model_str'] = 'Rand_Forest'
+    kwargs['model'] = RandomForestClassifier
+    kwargs['params'] = dict_params 
+    vary_parameters_model(X, y, kwargs)
+
+def vary_parameters_adaboost(X, y, dataset_name, dict_params):
+    kwargs = dict()
+    kwargs['dataset_name'] = dataset_name
+    kwargs['model_str'] = 'AdaBoost'
+    kwargs['model'] = AdaBoostClassifier
+    kwargs['params'] = dict_params 
+    vary_parameters_model(X, y, kwargs)
+
+def vary_parameters_gradient_boost(X, y, dataset_name, dict_params):
+    kwargs = dict()
+    kwargs['dataset_name'] = dataset_name
+    kwargs['model_str'] = 'GradientBoost'
+    kwargs['model'] = GradientBoostingClassifier
+    kwargs['params'] = dict_params 
+    vary_parameters_model(X, y, kwargs)
 
 def timeit(f):
     def timed(*args, **kwargs):
@@ -144,17 +165,31 @@ def timeit(f):
         return ret
     return timed
 
-def run_tests(dataset_name, dir_data):
-    X, y = prepare_X_y(dataset_name, dir_data)
-    if X is None:
-        return
-    timeit(test_parameters_random_forest)(dataset_name, X, y)
-    timeit(test_parameters_adaboost)(dataset_name, X, y)
-
 def yaml_loader(filepath):
     with open(filepath, 'r') as f:
         data = yaml.load(stream=f, Loader=yaml.FullLoader)
     return data
+
+def run_tests(dataset_name, dir_data):
+    X, y = prepare_X_y(dataset_name, dir_data, balanced_dataset=True)
+    if X is None:
+        print('error while preparing dataset')
+        return
+
+    dict_params = dict()
+    dict_params['n_estimators'] = np.arange(50,151,50)
+    dict_params['max_depth'] = np.arange(6,17,2)
+    timeit(vary_parameters_random_forest)(X, y, dataset_name, dict_params)
+
+    dict_params = dict()
+    dict_params['n_estimators'] = np.arange(60,161,20)
+    dict_params['learning_rate'] = np.arange(0.5,1.6,0.5)
+    timeit(vary_parameters_adaboost)(X, y, dataset_name, dict_params)
+
+    dict_params = dict()
+    dict_params['n_estimators'] = np.arange(60,161,20)
+    dict_params['learning_rate'] = np.arange(0.5,1.6,0.5)
+    timeit(vary_parameters_gradient_boost)(X, y, dataset_name, dict_params)
 
 if __name__ == '__main__':
     # dataset_name = 'cancer'
